398ab1aef4a172fd22b672633a40000390a2fb27
block <869938879@qq.com>
2014-10-13 10:35:24 +0800

    pod

---
+++ KBLove/Libs/ZCZBarViewController.h
@@ -0,0 +1,27 @@
+#import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+#import "ZBarReaderController.h"
+#define IOS7 [[[UIDevice currentDevice] systemVersion]floatValue]>=7
+@interface ZCZBarViewController : UIViewController<AVCaptureVideoDataOutputSampleBufferDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate,ZBarReaderDelegate,AVCaptureMetadataOutputObjectsDelegate>
+{
+    int num;
+    BOOL upOrdown;
+    NSTimer * timer;
+    UIImageView*_line;
+}
+
+
+@property (nonatomic,strong) AVCaptureVideoPreviewLayer *captureVideoPreviewLayer;
+@property (nonatomic, strong) AVCaptureSession *captureSession;
+
+@property (nonatomic, assign) BOOL isScanning;
+
+@property (nonatomic,copy)void(^ScanResult)(NSString*result,BOOL isSucceed);
+//初始化函数
+-(id)initWithBlock:(void(^)(NSString*,BOOL))a;
+
+//正则表达式对扫描结果筛选
++(NSString*)zhengze:(NSString*)str;
+
+
+@end
