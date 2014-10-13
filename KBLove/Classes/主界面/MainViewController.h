1b8dd0a1c68309da8e27d82fa318a6e37a0ab696
block <869938879@qq.com>
2014-10-13 23:34:06 +0800

    .

---
+++ KBLove/Classes/主界面/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  KBLove
+//
+//  Created by block on 14-10-13.
+//  Copyright (c) 2014年 block. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
--- KBLove/Classes/引导界面/登陆/LoginViewController.m
+++ KBLove/Classes/引导界面/登陆/LoginViewController.m
@@ -57,7 +57,6 @@
         [SVProgressHUD showWithStatus:@"登录中..." maskType:SVProgressHUDMaskTypeBlack];
         [[LoginRequest shareInstance] requestWithUserName:_userNameTF.text andPassWord:_passWordTF.text andLoginFinishedBlock:^{
             //成功后跳转
-#warning 登陆后跳转
             [SVProgressHUD dismiss];
             NSLog(@"登陆成功");
         } andLoginFaildeBlock:^(NSString *desc) {
