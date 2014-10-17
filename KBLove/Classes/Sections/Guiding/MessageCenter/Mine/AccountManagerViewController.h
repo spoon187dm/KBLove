c1abe9366cbd942999c7cb01b810d9ca388221f4
wucanqi <513852807@qq.com>
2014-10-17 14:36:12 +0800

    保存一下

---
+++ KBLove/Classes/Sections/Guiding/MessageCenter/Mine/AccountManagerViewController.h
@@ -0,0 +1,13 @@
+//
+//  AccountManagerViewController.h
+//  KBLove
+//
+//  Created by 吴铭博 on 14-10-17.
+//  Copyright (c) 2014年 block. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AccountManagerViewController : UIViewController
+
+@end
