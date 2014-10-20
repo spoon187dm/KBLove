b296b0a5ccd83b8c90b8d70bf77eb3d0c5acf426
block <869938879@qq.com>
2014-10-20 09:17:09 +0800

    1

--- KBLove/Classes/AppDelegate/AppDelegate.m
+++ KBLove/Classes/AppDelegate/AppDelegate.m
@@ -21,11 +21,11 @@
     [[UITableViewCell appearance]setBackgroundColor:[UIColor colorWithRed:0.000 green:0.569 blue:0.588 alpha:1.000]];
 //    [[UIView appearance]setBackgroundColor:SYSTEM_COLOR];
     [[UILabel appearance]setTextColor:[UIColor whiteColor]];
-    UIStoryboard *stb = [UIStoryboard storyboardWithName:@"Regist_Login_Storyboard" bundle:nil];
-    UIViewController *vc = [stb instantiateViewControllerWithIdentifier:@"WelcomeViewController"];
+//    UIStoryboard *stb = [UIStoryboard storyboardWithName:@"Regist_Login_Storyboard" bundle:nil];
+//    UIViewController *vc = [stb instantiateViewControllerWithIdentifier:@"WelcomeViewController"];
     
-//    UIStoryboard *stb = [UIStoryboard storyboardWithName:@"MainStoryboard" bundle:nil];
-//    UIViewController *vc = [stb instantiateViewControllerWithIdentifier:@"MainNavigationViewController"];
+    UIStoryboard *stb = [UIStoryboard storyboardWithName:@"MainStoryboard" bundle:nil];
+    UIViewController *vc = [stb instantiateViewControllerWithIdentifier:@"AlarmListViewController"];
 //    [self presentViewController:vc animated:YES completion:^{
 //        
 //    }];
