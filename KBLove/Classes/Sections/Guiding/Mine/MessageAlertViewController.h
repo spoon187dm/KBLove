b40477ef26b86f98a98baa92fe0f834304075b9d
wucanqi <513852807@qq.com>
2014-10-20 16:27:38 +0800

    123

---
+++ KBLove/Classes/Sections/Guiding/Mine/MessageAlertViewController.h
@@ -0,0 +1,13 @@
+//
+//  MessageAlertViewController.h
+//  KBLove
+//
+//  Created by 吴铭博 on 14-10-17.
+//  Copyright (c) 2014年 block. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MessageAlertViewController : UIViewController
+
+@end
