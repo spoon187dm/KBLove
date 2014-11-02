7ac30ac482d497b438834a61290a0d6db994055c
qianfeng <qianfeng@YLL.local>
2014-11-02 15:17:57 +0800

    zoomin   zoomout

--- KBLove/Classes/Sections/Main/Controllers/DeviceDetailViewController.m
+++ KBLove/Classes/Sections/Main/Controllers/DeviceDetailViewController.m
@@ -15,6 +15,9 @@
 {
     BMKMapView *baidu_MapView;
     MAMapView *gaode_MapView;
+    BMKPointAnnotation *baidu_SelfLocation;
+    MAPointAnnotation *gaode_SelfLocation;
+
 }
 @end
 
@@ -52,41 +55,29 @@
 }
 -(void)mapViewLoad
 {
-//    baidu_MapView=[[BMKMapView alloc]init];
-//    UIScreen *screen=[UIScreen mainScreen];
-//    baidu_MapView.frame=CGRectMake(0, 0, screen.bounds.size.width, screen.bounds.size.height-46);
-//    [self.view addSubview:baidu_MapView];
-//    baidu_MapView.delegate=self;
-//    
-//    
-//    BMKPointAnnotation *p=[[BMKPointAnnotation alloc]init];
-//    CLLocationCoordinate2D coor;
-//    coor.latitude = 39.915;
-//    coor.longitude = 116.404;
-//    p.coordinate = coor;
-//    p.title = @"小小酥";
-//    p.subtitle = @"12：30北京市海淀区创业大厦!";
-//    [baidu_MapView addAnnotation:p];
-//    CLLocationCoordinate2D cl2d =CLLocationCoordinate2DMake(40.035139, 116.311655);
-//    BMKPointAnnotation *point=[[BMKPointAnnotation alloc]init];
-//    point.coordinate=cl2d;
-//    [baidu_MapView addAnnotation:point];
-    
-    /**
-     高德地图
-     */
-    gaode_MapView=[[MAMapView alloc]init];
-    UIScreen *mainscreen=[UIScreen mainScreen];
-    gaode_MapView.frame=CGRectMake(0, 0,mainscreen.bounds.size.width, mainscreen.bounds.size.height-46);
-    [self.view addSubview:gaode_MapView];
-    gaode_MapView.delegate=self;
-    
-    
-    MAPointAnnotation *p=[[MAPointAnnotation alloc]init];
+    UIView *blank=[self.view viewWithTag:123];
+
+    if ([[[KBUserInfo sharedInfo]mapTypeName] isEqualToString:kMapTypeBaiduMap]) {
+        //百度地图
+        baidu_MapView=[[BMKMapView alloc]init];
+        UIScreen *screen=[UIScreen mainScreen];
+        baidu_MapView.frame=CGRectMake(0, 0, screen.bounds.size.width, screen.bounds.size.height-46);
+        [blank addSubview:baidu_MapView];
+        baidu_MapView.delegate=self;
+    }else {
+        /**
+         高德地图
+         */
+        gaode_MapView=[[MAMapView alloc]init];
+        UIScreen *mainscreen=[UIScreen mainScreen];
+        gaode_MapView.frame=CGRectMake(0, 0,mainscreen.bounds.size.width, mainscreen.bounds.size.height-46);
+        [blank addSubview:gaode_MapView];
+        gaode_MapView.delegate=self;
+    }
     CLLocationCoordinate2D coor;
     coor.latitude = 39.915;
     coor.longitude = 116.404;
-    [self addAnnotationViewToMap:coor titleStr:@"小小酥" subtitle:@"12：30北京市海淀区创业大厦"];
+    [self addAnnotationViewToMap:coor titleStr:@"小小酥" subtitle:@"12：30北京市海淀区创业大厦" isMyselfLocation:NO];
     
 }
 
@@ -94,24 +85,28 @@
 {
     static NSString* annotationIdentifier = @"warningPin";
     if ([annotation isKindOfClass:[BMKPointAnnotation class]]) {
-        //        if (annotation == Point) {
         BMKPinAnnotationView* annView = [[BMKPinAnnotationView alloc] initWithAnnotation:annotation
                                                                          reuseIdentifier:annotationIdentifier];
+        if (annotation ==baidu_SelfLocation) {
+            annView.image =  [UIImage imageNamed:@"car_warning.png"];
+            return annView;
+        }
         annView.image =  [UIImage imageNamed:@"car.png"];
         return annView;
-        //        }
     }
     if ([annotation isKindOfClass:[MAPointAnnotation class]]) {
-        //        if (annotation == Point) {
-
+  
         MAAnnotationView * annView = [[MAAnnotationView alloc] initWithAnnotation:annotation
                                                                         reuseIdentifier:annotationIdentifier];
+        if (annotation ==gaode_SelfLocation) {
+            annView.image =  [UIImage imageNamed:@"car.png"];
+            return annView;
+        }
         annView.image =  [UIImage imageNamed:@"endPoint.png"];
         
         annView.canShowCallout               = YES;
         annView.draggable                    = YES;
         return annView;
-        //        }
     }
     return nil;
 }
@@ -121,17 +116,37 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
--(void)addAnnotationViewToMap:(CLLocationCoordinate2D)coordinate titleStr:(NSString *)title subtitle:(NSString *)subtitle
+-(void)addAnnotationViewToMap:(CLLocationCoordinate2D)coordinate titleStr:(NSString *)title subtitle:(NSString *)subtitle  isMyselfLocation:(Boolean)type
 {
-    if (1) {
+    if ([[[KBUserInfo sharedInfo]mapTypeName] isEqualToString:kMapTypeBaiduMap]) {
+        if (type) {
+            baidu_SelfLocation=[[BMKPointAnnotation alloc]init];
+            baidu_SelfLocation.coordinate=coordinate;
+            baidu_SelfLocation.title=title;
+            baidu_SelfLocation.subtitle=subtitle;
+            [baidu_MapView addAnnotation:baidu_SelfLocation];
+            return;
+        }
+        BMKPointAnnotation *p=[[BMKPointAnnotation alloc]init];
+        p.coordinate = coordinate;
+        p.title = title;
+        p.subtitle =subtitle;
+        [baidu_MapView addAnnotation:p];
+    }else{
+        if (type) {
+            gaode_SelfLocation=[[MAPointAnnotation alloc]init];
+            gaode_SelfLocation.coordinate=coordinate;
+            gaode_SelfLocation.title=title;
+            gaode_SelfLocation.subtitle=subtitle;
+            [gaode_MapView addAnnotation:gaode_SelfLocation];
+            return;
+        }
         MAPointAnnotation *p=[[MAPointAnnotation alloc]init];
         p.coordinate = coordinate;
         p.title = title;
         p.subtitle =subtitle;
         [gaode_MapView addAnnotation:p];
     }
-    
-//    [baidu_MapView addAnnotation:p];
 }
 
 #pragma mark -
@@ -145,23 +160,32 @@
 //定位
 - (IBAction)click_lacation:(UIButton *)sender
 {
-    
+    CLLocationCoordinate2D coor;
+    coor.latitude = 39.815;
+    coor.longitude = 116.374;
+    [self addAnnotationViewToMap:coor titleStr:@"当前位置" subtitle:@"当前位置"  isMyselfLocation:YES];
 }
 //地图放大
 - (IBAction)click_zoomin:(UIButton *)sender
 {
-    float zoomlevel=baidu_MapView.zoomLevel;
-    baidu_MapView.zoomLevel=++zoomlevel;
-//    float zoomlevel=gaode_MapView.zoomLevel;
-//    gaode_MapView.zoomLevel=++zoomlevel;
+    if ([[[KBUserInfo sharedInfo]mapTypeName] isEqualToString:kMapTypeBaiduMap]) {
+        float zoomlevel=baidu_MapView.zoomLevel;
+        baidu_MapView.zoomLevel=++zoomlevel;
+    }else{
+        float zoomlevel=gaode_MapView.zoomLevel;
+        gaode_MapView.zoomLevel=++zoomlevel;
+    }
 }
 //地图缩小
 - (IBAction)click_zoomout:(UIButton *)sender
 {
-    float zoomlevel=baidu_MapView.zoomLevel;
-    baidu_MapView.zoomLevel=++zoomlevel;
-//    float zoomlevel=gaode_MapView.zoomLevel;
-//    gaode_MapView.zoomLevel=++zoomlevel;
+    if ([[[KBUserInfo sharedInfo]mapTypeName] isEqualToString:kMapTypeBaiduMap]) {
+        float zoomlevel=baidu_MapView.zoomLevel;
+        baidu_MapView.zoomLevel=--zoomlevel;
+    }else{
+        float zoomlevel=gaode_MapView.zoomLevel;
+        gaode_MapView.zoomLevel=--zoomlevel;
+    }
 }
 
 - (IBAction)click_back:(UIButton *)sender {
