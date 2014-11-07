75520b608ae79c6250df48cd3dce21b14252bdbb
douglas525264 <13520580107@163.com>
2014-11-07 11:24:03 +0800

    ,

---
+++ KBLove/Classes/General/Categories/UITableViewController+WLTool.h
@@ -0,0 +1,18 @@
+//
+//  UITableViewController+WLTool.h
+//  KBLove
+//
+//  Created by block on 14/11/5.
+//  Copyright (c) 2014年 block. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITableViewController (WLTool)
+
+/**
+ 将self.view从UITableView修改为uiview。
+ */
+- (void)replaceSelfViewToNormal;
+
+@end
